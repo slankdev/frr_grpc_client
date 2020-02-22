be7c3668f4ed5acf9a2ac175b8a77b46a81c524a
Hiroki Shirokura <slank.dev@gmail.com>
2020-02-22 16:41:02 +0000

    2020.02.22-16:41

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	//"context"
-	//"os"
-	//"time"
-	"log"
+	"io"
 	"fmt"
+	"time"
+	"context"
 	"google.golang.org/grpc"
 	pb "github.com/slankdev/frr_grpc_client/frr"
 )
@@ -19,10 +18,46 @@ func main() {
 
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		panic(err)
 	}
 	defer conn.Close()
 	c := pb.NewNorthboundClient(conn)
 
-	fmt.Printf("slankdev: %+v\n", c)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.GetCapabilities(ctx, &pb.GetCapabilitiesRequest{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("frr_version: %+v\n", r.FrrVersion)
+	fmt.Printf("rollback_support: %+v\n", r.RollbackSupport)
+	for i, module := range(r.SupportedModules) {
+		fmt.Printf("module[%d]: %+v\n", i, module)
+	}
+	for i, encoding := range(r.SupportedEncodings) {
+		fmt.Printf("encoding[%d]: %+v\n", i, encoding)
+	}
+	fmt.Printf("--------------------\n")
+
+	// path := []string{"/frr-interface","lib/frr-interface","interface/frr-interface","nam"}
+	path := []string{"lib/frr-interface"}
+	// path := []string{"/frr-ripd:clear-rip-route"}
+	stream, err := c.Get(ctx, &pb.GetRequest{Type:pb.GetRequest_ALL, Encoding:pb.Encoding_JSON, Path:path})
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("RES: %+v\n", res);
+	}
 }
